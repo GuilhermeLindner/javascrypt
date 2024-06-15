5686853d107bbb1adfcc80f76819460d398e5b43
GuilhermeLindner <glindnerp@gmail.com>
2024-06-14 23:23:43 -0300

    condições

---
+++ Exercicios/ex008/index.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`a velocidade do carro é ${vel} km`)
+if (vel > 60) {
+    console.log('Vcoê ultrapasssou a velocidade permitida. MULTADO!!!') //condição simples
+}
+console.log('dirija sempre com cinto')
