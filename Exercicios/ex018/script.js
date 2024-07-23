b753b42208cc4559dd4dbec7da2406d6d6bd6d7a
GuilhermeLindner <glindnerp@gmail.com>
2024-07-22 21:27:35 -0300

    pera

---
+++ Exercicios/ex018/script.js
@@ -0,0 +1,8 @@
+function verificar() {
+    var c = document.getElementById('dois')
+    var i = document.getElementById('um')
+    var s = document.getElementById('salt')  
+    do {
+        
+    } 
+}
